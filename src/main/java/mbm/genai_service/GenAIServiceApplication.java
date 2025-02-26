acce61ec0fe18d9dd395c58868d6e0f9862401d3
melihburakmert <melih.burak.mert@gmail.com>
2025-02-26 11:01:18 +0100

    Initial commit

---
+++ src/main/java/mbm/genai_service/GenAIServiceApplication.java
@@ -0,0 +1,13 @@
+package mbm.genai_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAIServiceApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(GenAIServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mbm/genai_service/GenAIServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package mbm.genai_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class GenAIServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
