2d47f11c27b402b0dcb50ca9a267a5f53b588e39
melihburakmert <melih.burak.mert@gmail.com>
2025-02-28 17:21:58 +0100

    Verify modulith

--- src/test/java/mbm/genai_service/GenAIServiceApplicationTests.java
+++ src/test/java/mbm/genai_service/GenAIServiceApplicationTests.java
@@ -2,6 +2,8 @@ package mbm.genai_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -10,6 +12,14 @@ class GenAIServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void writeDocumentationSnippets() {
+		final var modules = ApplicationModules.of(GenAIServiceApplication.class).verify();
+		modules.forEach(System.out::println);
+
+		new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
 	}
 
 }
